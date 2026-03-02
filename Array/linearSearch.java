b36200bce1873c4c7ba6aaac70beece9377e420f
Nihal Dangi <nihalthakur281@gmail.com>
2026-03-02 22:40:35 +0530

    Add files via upload

---
+++ Array/linearSearch.java
@@ -0,0 +1,47 @@
+// public class linearSearch {
+//     public static int linearSearch(int numbers[], int key){
+
+//         for(int i = 0; i<numbers.length; i++) {
+//             if(numbers[i] == key) {
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+//     }
+//     public static void main(String[] args) {
+//         int numbers[] = {2, 4, 6, 8, 10, 12, 14};
+//         int key = 10;
+
+//         int index = linearSearch(numbers, key);
+//         if(index == -1) {
+//             System.out.println("NOT found");
+//         } else {
+//             System.out.println("key is at index : " + index);
+//         }
+//     }
+// }
+
+//Largest Number in Array
+
+public class linearSearch {
+    public static int getLargest(int numbers[]) {
+        int largest = Integer.MIN_VALUE; //-infinity
+        int smallest = Integer.MAX_VALUE; //+infinity
+
+        for(int i = 0; i<numbers.length; i++) {
+            if(largest < numbers[i]) {
+                largest = numbers[i];
+            }
+            if(smallest > numbers[i]) {
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("smallest value is :" + smallest);
+        return largest;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {1, 3, 2, 5, 6, 4};
+        System.out.println("larest value is :" + getLargest(numbers));
+    }
+}

1d3d92c6f154df8faf9550ea3fae7be0d0052f6b
Nihal Dangi <nihalthakur281@gmail.com>
2026-03-15 14:15:47 +0530

    Add files via upload

---
+++ Array/buyandsellStocks.java
@@ -0,0 +1,25 @@
+//Best time to buy and sell Stocks.
+
+public class buyandsellStocks {
+
+    public static int buyAndSellStocks(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0; i<prices.length; i++) {
+            if(buyPrice < prices[i]) { //profit
+                int profit = prices[i] - buyPrice; //today's profit
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = {7, 1, 5, 3, 6, 4};
+        System.err.println(buyAndSellStocks(prices));
+    }
+}
